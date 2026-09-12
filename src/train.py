6db09e7345c1ea31731f23802291502fae568e6d
charitha bandara <charithabandara@charithas-MacBook-Air.local>
2026-09-12 23:55:51 +0530

    Implement MLflow experiment tracking

--- src/train.py
+++ src/train.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import joblib
+import mlflow
+import mlflow.sklearn
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -25,24 +27,46 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state=42
 )
 
-print("Training model...")
+# Create MLflow experiment
+mlflow.set_experiment("House Price Prediction")
 
-# Train model
-model = LinearRegression()
-model.fit(X_train, y_train)
+with mlflow.start_run():
 
-# Predict
-predictions = model.predict(X_test)
+    print("Training model...")
 
-# Evaluate
-r2 = r2_score(y_test, predictions)
-rmse = mean_squared_error(y_test, predictions) ** 0.5
+    # Train model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
 
-print(f"R² Score: {r2:.4f}")
-print(f"RMSE: {rmse:.2f}")
+    # Predict
+    predictions = model.predict(X_test)
 
-# Save model
-joblib.dump(model, "models/model.pkl")
+    # Evaluate
+    r2 = r2_score(y_test, predictions)
+    rmse = mean_squared_error(y_test, predictions) ** 0.5
 
-print("Model saved successfully!")
-print("Saved in models/model.pkl")
+    print(f"R² Score: {r2:.4f}")
+    print(f"RMSE: {rmse:.2f}")
+
+    # Log parameters
+    mlflow.log_param("model_type", "LinearRegression")
+    mlflow.log_param("test_size", 0.2)
+    mlflow.log_param("random_state", 42)
+
+    # Log metrics
+    mlflow.log_metric("r2_score", r2)
+    mlflow.log_metric("rmse", rmse)
+
+    # Save model locally
+    joblib.dump(model, "models/model.pkl")
+
+    # Log model to MLflow
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="house_price_model"
+    )
+
+    print("Model saved successfully!")
+    print("Saved in models/model.pkl")
+
+print("MLflow experiment logged successfully!")
