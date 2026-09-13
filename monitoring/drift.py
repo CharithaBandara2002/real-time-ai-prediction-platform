596b885418dc255b759866c6a54a0edafa6aef91
charitha bandara <charithabandara@charithas-MacBook-Air.local>
2026-09-13 15:10:25 +0530

    Complete data drift monitoring dashboard

--- monitoring/drift.py
+++ monitoring/drift.py
@@ -8,22 +8,21 @@ print("Loading datasets...")
 reference_data = pd.read_csv("data/raw/housing.csv")
 current_data = pd.read_csv("data/raw/current_data.csv")
 
-# Remove text column
 reference_data = reference_data.drop("Address", axis=1)
 current_data = current_data.drop("Address", axis=1)
 
 print("Generating drift report...")
 
 report = Report(
-    metrics=[
-        DataDriftPreset()
-    ]
+    metrics=[DataDriftPreset()]
 )
 
-report.run(
+result = report.run(
     reference_data=reference_data,
     current_data=current_data
 )
-print(dir(report))
+
+result.save_html("monitoring/drift_report.html")
 
 print("Drift report generated successfully!")
+print("Saved: monitoring/drift_report.html")
